e09821a2c19576f5a823a873719eb7e5b51679b4
benocode <benoithersant@gmail.com>
2023-02-15 13:14:17 +0100

    init

---
+++ src/test/java/com/openclassrooms/api/EmployeeControllerTest.java
@@ -0,0 +1,25 @@
+package com.openclassrooms.api;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeControllerTest {
+
+	@Autowired
+    private MockMvc mockMvc; // instanciation d'un attribut de la classe MockMvc pour appel à la méthode perform
+
+    @Test
+    public void testGetEmployees() throws Exception {
+        mockMvc.perform(get("/employees")) // déclenche la requête
+            .andExpect(status().isOk()); // attend une réponse http 200
+    }
+}
